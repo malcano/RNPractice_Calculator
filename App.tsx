b6a0ab862ceb238dcc52d63ed2c0e585389f7a49
마루우루 <marupedia@maluuluui-MacBookPro.local>
2025-01-12 18:34:14 +0900

    edit some codes

--- App.tsx
+++ App.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
+
+const NumberButton = styled.TouchableOpacity`
+background-color: #B6B6B6;
+border-radius: 100px;
+width: 100px;
+height: 100px;
+align-items: center;
+justify-content: center;
+`;
+
+const NumberText = styled.Text`
+  font-size: 30px;
+  color: white;
+  bold: true;
+`;
+
 const App = () => {
+  const buttons = Array.from({ length: 10 }, (_, i) => i);
+
   return (
-    <View style ={{backgroundColor: '#181818', flex: 1}}>
-      <Text>Calculator</Text>
+    <View style={{ backgroundColor: '#181818', flex: 1 }}>
+      <Text>0</Text>
+      {buttons.reverse().map((button) => (
+        <NumberButton key={button}>
+          <NumberText style={{color: 'white'}}>{button}</NumberText>
+        </NumberButton>
+      ))}
     </View>
   );
 };
